7f32f467dec29c642a0c583c26a4cd8cfa7e6275
shrey kataria <shreykataria1@gmail.com>
2024-05-17 14:38:35 +0530

    config mongodb connection and tested the app

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,26 @@
+const bodyParser = require("body-parser");
 const express = require("express");
+const { default: mongoose } = require("mongoose");
+const connectDB = require("./config/db");
 require("dotenv").config();
 
+// initialize app
 const app = express();
 
+// database
+connectDB();
+
+// port
 const port = process.env.PORT || 8000;
 
-app.use("/", (req, res, next) => {
-  res.send("peer-five");
+// middlewares
+app.use(bodyParser.json());
+
+// routes
+
+app.get("/", (req, res, next) => {
+  res.json({ message: "test-app" });
 });
 
+// server
 app.listen(port, () => console.log(`server started at port : ${port}`));
